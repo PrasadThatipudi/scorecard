a11863df8d950d31d2543b4d51adf2345cf514b8
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-01-14 23:07:25 +0530

    Update scorecard.js

--- scorecard.js
+++ scorecard.js
@@ -13,11 +13,6 @@ export const incrementOvers = ({ overs, balls }) => ({
   balls: (balls + 1) % 6,
 });
 
-export const scoreOfTheDelivery = (delivery) => delivery.runs.total;
-
-export const totalScoreOfDeliveries = (deliveries) =>
-  deliveries.map(scoreOfTheDelivery).reduce(add);
-
 export const getAllDeliveries = (overs) =>
   overs.flatMap(({ deliveries }) => deliveries);
 
@@ -49,24 +44,39 @@ export const updateBatterInfo = (batsman, delivery) => {
   return mergeWith(latest, batsman || batterTemplate(delivery.batter), add);
 };
 
+export const getDismissalData = (bowler, wicket) => ({
+  bowler,
+  kind: wicket.kind,
+  fielders: wicket.fielders || [],
+});
+
 export const insertDismissalData = (batter, delivery) => {
   const wicket = delivery.wickets[0];
-  const dismissal = wicketKind[wicket.kind](delivery.bowler, wicket.fielders);
 
-  return { ...(batter || batterTemplate(wicket.player_out)), dismissal };
+  return { ...batter, dismissal: getDismissalData(delivery.bowler, wicket) };
 };
 
 const isWicket = (delivery) => "wickets" in delivery;
 const isRunOut = (wicket) => wicket.kind === "run out";
 
-export const updateBowlerInfo = (bowler, delivery) => {
-  const latest = {
-    overs: isFairDelivery(delivery) ? 1 : 0,
-    runs: delivery.runs.total,
-    wickets: isWicket(delivery) && !isRunOut(delivery.wickets[0]) ? 1 : 0,
-  };
+export const bowlerStatsOfDelivery = (delivery) => ({
+  runs: delivery.runs.total,
+  wickets: isWicket(delivery) && !isRunOut(delivery.wickets[0]) ? 1 : 0,
+});
 
-  return mergeWith(latest, bowler || bowlerTemplate(delivery.bowler), add);
+export const updateBowlerInfo = (bowler, delivery) => {
+  const newStats = bowlerStatsOfDelivery(delivery);
+  const bowlerStats = { ...(bowler || bowlerTemplate(delivery.bowler)) };
+  const overs = isFairDelivery(delivery)
+    ? incrementOvers(bowlerStats.overs)
+    : bowlerStats.overs;
+
+  return {
+    ...bowlerStats,
+    overs: overs,
+    runs: bowlerStats.runs + newStats.runs,
+    wickets: bowlerStats.wickets + newStats.wickets,
+  };
 };
 
 export const updateExtras = (extras, delivery) =>
@@ -117,24 +127,12 @@ export const updateScoreCard = (info, delivery) => {
   return scoreSheet;
 };
 
-const ballsToOvers = (balls) => Math.floor(balls / 6) + (balls % 6) / 10;
-
 export const getScoreCard = ({ team, overs }) => {
   const deliveries = getAllDeliveries(overs);
 
   const finalScoreCard = deliveries.reduce(
     (scoreCard, delivery) => updateScoreCard(scoreCard, delivery),
-    scoreCardTemplate()
-  );
-
-  finalScoreCard.teamInfo.team = team;
-
-  Object.entries(finalScoreCard.bowlers).forEach(
-    ([bowler, bowlerStats]) =>
-      (finalScoreCard.bowlers[bowler] = {
-        ...bowlerStats,
-        overs: ballsToOvers(bowlerStats.overs),
-      })
+    scoreCardTemplate(team)
   );
 
   return finalScoreCard;
